1b0aa2b561003a7e435054bc7d50371a745ac7a5
Carlos Castro <alu0100819847@ull.edu.es>
2019-09-23 19:32:35 +0200

    Class UserTest added #7

---
+++ src/test/java/es/upm/miw/iwvg/practica/UserTest.java
@@ -0,0 +1,61 @@
+package es.upm.miw.iwvg.practica;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserTest {
+
+    private User user;
+
+    @BeforeEach
+    void before() {
+        user = new User(1, "Carlos", "Castro");
+    }
+
+    @Test
+    void testFullName() {
+        assertEquals("Carlos Castro", user.fullName());
+        assertNotEquals("Antonio Alvarez", user.fullName());
+    }
+
+    @Test
+    void testInitials() {
+        assertEquals("C.", user.initials());
+        assertNotEquals("Carlos", user.initials());
+    }
+
+    @Test
+    void testGetNumber() {
+        assertEquals(1, user.getNumber());
+        assertNotEquals(5, user.getNumber());
+    }
+
+    @Test
+    void testGetName() {
+        assertEquals("Carlos", user.getName());
+        assertNotEquals("Antonio", user.getName());
+    }
+
+    @Test
+    void testGetFamilyName() {
+        assertEquals("Castro", user.getFamilyName());
+        assertNotEquals("Alvarez", user.getFamilyName());
+    }
+
+    @Test
+    void testFormat() {
+        user = new User(1, "carlos", "castro");
+        assertEquals("Carlos Castro", user.fullName());
+        assertNotEquals("carlos castro", user.fullName());
+        assertEquals("Carlos", user.getName());
+        assertNotEquals("carlos", user.getName());
+        assertEquals("C.", user.initials());
+        assertNotEquals("c.", user.initials());
+
+    }
+
+}
